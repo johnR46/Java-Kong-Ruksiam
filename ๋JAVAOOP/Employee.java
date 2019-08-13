8142d6aefcd870aacab3f5c60b072a93ada48a3e
JohnR46 <Machine.The@hotmail.com>
2019-08-13 21:59:30 +0700

    part 4 setter

--- ๋JAVAOOP/Employee.java
+++ ๋JAVAOOP/Employee.java
@@ -4,4 +4,13 @@ class Employee {
     private String name;
     private double salary;
 
+    public void setId(String id) {
+        this.id = id; // this ดึง private String id มาใช้ 
+
+    }
+
+    public void setName(String name) {
+        this.name = name; //this ดึง private String name มาใช้ 
+    }
+
 }
--- ๋JAVAOOP/Main.java
+++ ๋JAVAOOP/Main.java
@@ -3,5 +3,16 @@ public class Main {
 
         Employee e1 = new Employee(); // Create Object
 
+        e1.setId("1"); 
+        e1.setName("John Nonphala");
+      //  e1.salary = 30000.32;
+
+
+
+        System.out.println("ID = "+ e1.id);
+        
+        System.out.println("NAME = "+ e1.name);
+        
+       // System.out.println("SALARY = "+ e1.salary);
     }
 }
